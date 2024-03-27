aebaeaaad04f5100339bec085c499c09986cf1bf
Faraz1306 <farazsiddiqui02@yahoo.com>
2024-03-27 16:31:12 +0100

    service tournoi

---
+++ src/app/tournois.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TournoisService {
+
+  constructor( private http: httpClient ) {  }
+
+}
