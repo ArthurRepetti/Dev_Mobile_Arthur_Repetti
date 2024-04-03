01cd41d698505246f49fd19942876ee711d2d11f
ArthuroofGangstat <arthur.repetti@gmail.com>
2024-04-03 09:29:50 +0200

    ajout commentaires pour personne et tournois

--- src/app/Personne.ts
+++ src/app/Personne.ts
@@ -1,10 +1,10 @@
 
 export interface Personne {
-  _id: number;
-  date_naissance: string;
-  nom: string;
-  prenom: string;
-  pseudo: string;
-  sexe: string;
+  _id: number; // Identifiant de la personne (nombre)
+  date_naissance: string; // La date de naissance de la personne (string)
+  nom: string; // Le nom de la personne (string)
+  prenom: string; // Le prenom de la personne (string)
+  pseudo: string; // Le pseudo de la personne (string)
+  sexe: string; // Le sexe de la personne (string)
 
 }
